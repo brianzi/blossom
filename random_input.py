3284f380e34985b83185a7c7a56d0a161acdb7a6
Brian Tarasinski <brianzi@physik.fu-berlin.de>
2017-01-09 14:30:05 +0100

    automatic testing script in python

--- random_input.py
+++ random_input.py
@@ -1,32 +1,49 @@
 """
-Make a random input file for blossom_alt.c
+Make a random input file for blossom2.c, run and check return value
 """
 import numpy as np
+
+import subprocess
 
 
 def random_bipartite(n=10):
 
-    pairs = [(i, i + n, np.random.randint(20)) for i in range(n)]
-    pairs += [(i, np.random.randint(n) + n, np.random.randint(20))
-              for i in range(n) for j in range(5)]
+    pairs = {}
+    for i in range(n):
+        pairs[frozenset([i, i + n])] = np.random.randint(20)
+    for i in range(n):
+        for _ in range(5):
+            j = np.random.randint(n)
+            pairs[frozenset([i, j + n])] = np.random.randint(20)
 
-    return pairs
+    return pairs.items()
 
 
 def random_non_bipartite(n=10):
-    pairs = {} 
+    pairs = {}
     for i in range(n):
         for _ in range(5):
             j = i
             while j == i:
                 j = np.random.randint(n)
-            pairs[frozenset([i, j])] =  np.random.randint(20)
+            pairs[frozenset([i, j])] = np.random.randint(10)
     return pairs.items()
 
 
-# pairs = random_bipartite()
-pairs = random_non_bipartite()
+def make_file(fn="rnd.dat", n=8):
+    with open(fn, "w") as f:
+        pairs = random_non_bipartite(n)
+        for s, w in pairs:
+            print(*tuple(s), w, file=f)
 
 
-for s, w in pairs:
-    print(*tuple(s), w)
+if __name__ == "__main__":
+    for i in range(200):
+        
+        make_file(n=10)
+        p = subprocess.run(["./a.out", "rnd.dat"], timeout=2, stdout=subprocess.DEVNULL)
+
+        if p.returncode != 0:
+            print("failed!")
+            break
+        print(i)
